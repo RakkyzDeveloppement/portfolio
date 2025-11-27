bea9d0bed1b875e4aed4dcfba911fb13d4289cfa
RakkyzDeveloppement <hugologer8867@gmail.com>
2025-11-27 23:37:04 +0100

    mise a jour du site

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const imagenav1 = document.getElementsByClassName('imagenav1');
+const imagenav2 = document.getElementsByClassName('imagenav2');
+const imagenav3 = document.getElementsByClassName('imagenav3');
+const btnprojets = document.getElementById('btnprojets');
+const btncontact = document.getElementById('btncontact');
 const btncompetences = document.getElementById('btncompetences');
 const competences = document.getElementById('competences');
 
-btncompetences.addEventListener('click', () => {
-    competences.classList.add('visible');
-});
 
+btncompetences.addEventListener('click', () => {
+    competences.classList.toggle('visible');
+});
