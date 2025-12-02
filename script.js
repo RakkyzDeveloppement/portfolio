174f8a355460fb2125add7560f87d05f42fa0387
RakkyzDeveloppement <hugologer8867@gmail.com>
2025-12-02 23:15:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const imagenav1 = document.getElementsByClassName('imagenav1');
 const imagenav2 = document.getElementsByClassName('imagenav2');
 const imagenav3 = document.getElementsByClassName('imagenav3');
+const espacepresentation = document.getElementsByClassName('espacepresentation');
 const btnprojets = document.getElementById('btnprojets');
 const btncontact = document.getElementById('btncontact');
 const btncompetences = document.getElementById('btncompetences');
@@ -14,4 +15,8 @@ btncompetences.addEventListener('click', () => {
 
 btnprojets.addEventListener('click', () => {
     carouselprojets.classList.toggle('visible');
-})
+});
+
+btncompetences.addEventListener('click', () => {
+    espacepresentation.classList.toggle('invisible');
+});
